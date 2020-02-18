6b595ed7bd3e0b749eb0a1cb9537764ee2e3652c
Xavier Carty <xaviercarty@gmail.com>
2020-02-18 15:17:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,4 +35,9 @@ def aoh_update(aoh, row, key, new_value)
 
 end
 
+
+xavier = [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}, {:name=>"Pat Monteseian", :occupat
+ion=>"Staff"}, {:name=>"Kate Gompert", :occupation=>"None"}, {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+
+print xavier[0][:name]
 print assembled_aoh
