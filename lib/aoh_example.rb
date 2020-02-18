9083375414147d67c38e8271d5f014e11f4528e2
Xavier Carty <xaviercarty@gmail.com>
2020-02-18 15:15:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,7 +24,7 @@ end
 def aoh_lookup(aoh, row, key)
 
   aoh[row][key]
-  
+
 end
 
 def aoh_update(aoh, row, key, new_value)
@@ -32,5 +32,5 @@ def aoh_update(aoh, row, key, new_value)
   # Return the updated AoH
    aoh[row][key] = new_value
    aoh
-  
+
 end
